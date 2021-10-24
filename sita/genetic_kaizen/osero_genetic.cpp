6e1f38950ebb001e6234c4fb433a3ab6a7db06d9
TTaisei <77839417+TTaisei@users.noreply.github.com>
2021-10-24 13:46:40 +0900

    Add files via upload

---
+++ sita/genetic_kaizen/osero_genetic.cpp
@@ -0,0 +1,304 @@
+#include "osero_genetic.h"
+
+extern const bool BLACK;
+extern const bool WHITE;
+
+int osero_genetic::play(void){
+    bool can = true, old_can = true;
+    int line, col;
+
+    srand(this -> srand_num);
+    void (osero_genetic:: * player[2]) (int *, int *) = {
+        play_method[this -> bmethod],
+        play_method[this -> wmethod]
+    };
+
+    if (this -> print) this -> printb();
+
+    while((can = check_all()) || old_can){    
+        if (can){
+            (this->*player[INT(this -> turn)])(&line, &col);
+            this -> put(this -> bw, line, col, this -> turn);
+            if (this -> print) this -> printb();
+        }
+        this -> turn = !(this -> turn);
+        old_can = can;
+    }
+
+    if (this -> mode)
+        return this -> count_last();
+    else
+        return 0;
+}
+
+void osero_genetic::random(int * line, int * col){
+    int x, y;
+
+    do{
+        x = rand() % this -> SIZE;
+        y = rand() % this -> SIZE;
+    } while (!(this -> check(this -> bw, x, y)));
+
+    *line = x, *col = y;
+}
+
+void osero_genetic::nhand(int * line, int * col){
+    int i = 0, j = 0;
+    double score = 0, max_score = -(this -> SIZE << 3);
+    int line_ans[this -> SIZE << 1], col_ans[this -> SIZE << 1];
+    int num = 0;
+    BOARD place = 1;
+    BOARD board_leaf[2];
+
+    while (place){
+        if (check(this -> bw, i, j)){
+            board_leaf[0] = this -> bw[0];
+            board_leaf[1] = this -> bw[1];
+            put(board_leaf, i, j, this -> turn);
+            score = this -> board_add(board_leaf, 1, !(this -> turn));
+            if (score == -100.0) score = count(board_leaf);
+            if (score > max_score){
+                max_score = score;
+                line_ans[0] = i;
+                col_ans[0] = j;
+                num = 0;
+            }else if (score == max_score){
+                num++;
+                line_ans[num] = i;
+                col_ans[num] = j;
+            }
+        }
+        i++;
+        if (i == 8) i = 0, j++;
+        place = place << 1;
+    }
+
+    if (num) {
+        int place = rand() % (num + 1);
+        line_ans[0] = line_ans[place];
+        col_ans[0] = col_ans[place];
+    }
+
+    *line = line_ans[0];
+    *col = col_ans[0];
+}
+
+double osero_genetic::board_add(BOARD * now, int num, bool turn){
+    if (num == this -> read_goal[INT(this -> turn)]) return count(now);
+
+    int i = 0, j = 0, put_num = 0;
+    double score_ele, score = 1.0;
+    BOARD place = 1;
+    BOARD board_leaf[2];
+
+    while (place){
+        if (check(now, i, j)){
+            board_leaf[0] = now[0];
+            board_leaf[1] = now[1];
+            put(board_leaf, i, j, turn);
+            score_ele = this -> board_add(board_leaf, num + 1, !turn);
+            if (score_ele == -100.0) score_ele = count(now);
+            score += score_ele;
+            put_num++;
+        }
+        i++;
+        if (i == 8) i = 0, j++;
+        place = place << 1;
+    }
+
+    if (put_num) return score / put_num;
+    else return -100.0;
+}
+
+double osero_genetic::count(BOARD * now){
+    int my = INT(this -> turn);
+    int opp = INT(!(this -> turn));
+    int i = 0;
+    double score = 0;
+    double * myeva = this -> eva[my];
+    BOARD place = 1;
+
+    while (place){
+        if (now[my] & place)
+            score += myeva[i];
+        else if (now[opp] & place)
+            score -= myeva[i];
+        i++;
+        place = place << 1;
+    }
+
+    return score;
+}
+
+int osero_genetic::count_last(void){
+    int my, opp;
+
+    my = this -> computer;
+    opp = this -> player;
+
+    my = popcount(this -> bw[my]);
+    opp = popcount(this -> bw[opp]);
+
+    return my - opp;
+}
+
+int osero_genetic::popcount(BOARD now){
+    now = now - ((now >> 1) & 0x5555555555555555);
+    now = (now & 0x3333333333333333) + ((now >> 2) & 0x3333333333333333);
+    now = (now + (now >> 4)) & 0x0f0f0f0f0f0f0f0f;
+    now = now + (now >> 8);
+    now = now + (now >> 16);
+    now = now + (now >> 32);
+
+    return now & 0x7f;
+}
+
+bool osero_genetic::check_all(void){
+    int i, j;
+
+    for (i = 0; i < SIZE; i++)
+        for (j = 0; j < SIZE; j++)
+            if (this -> check(this -> bw, i, j)) return true;
+
+    return false;
+}
+
+bool osero_genetic::check(BOARD * now, int line, int col){
+    if (now[INT(TURN::black)] & static_cast<BOARD>(1) << ((line << 3) + col)) return false;
+    if (now[INT(TURN::white)] & static_cast<BOARD>(1) << ((line << 3) + col)) return false;
+
+    int x, y;
+    int line_x, col_y;
+    int my = INT(this -> turn),
+        opp = INT(!(this -> turn));
+
+    for (x = -1; x <= 1; x++){
+        for (y = -1; y <= 1; y++){
+            if (x != 0 || y != 0){
+                line_x = line + x;
+                col_y = col + y;
+                while ((now[opp] & static_cast<BOARD>(1) << ((line_x << 3) + col_y))
+                       && 0 <= x + line_x && x + line_x < this -> SIZE
+                       && 0 <= y + col_y && y + col_y < this -> SIZE){
+                    if (now[my] & static_cast<BOARD>(1) << ((line_x + x << 3) + col_y + y))
+                        return true;
+                    line_x += x;
+                    col_y += y;
+                }
+            }
+        }
+    }
+
+    return false;
+}
+
+void osero_genetic::put(BOARD * now, int line, int col, bool turn){
+    int x, y;
+    int line_x, col_y, num;
+    bool my = turn, opp = !turn;
+    BOARD inver, place;
+
+    now[my] = now[my] | static_cast<BOARD>(1) << ((line << 3) + col); 
+
+    for (x = -1; x <= 1; x++){
+        for (y = -1; y <= 1; y++){
+            if (x != 0 || y != 0){
+                inver = 0;
+                line_x = line + x;
+                col_y = col + y;
+                place = static_cast<BOARD>(1) << ((line_x << 3) + col_y);
+                while ((now[opp] & place)
+                       && 0 <= x + line_x && x + line_x < this -> SIZE
+                       && 0 <= y + col_y && y + col_y < this -> SIZE){
+                    inver = inver | place;
+                    line_x += x;
+                    col_y += y;
+                    place = static_cast<BOARD>(1) << ((line_x << 3) + col_y);
+                }
+                if (now[my] & place){
+                    now[my] = now[my] + inver;
+                    now[opp] = now[opp] - inver;
+                }
+            }
+        }
+    }
+}
+
+void osero_genetic::printb(void){
+    BOARD place = 1;
+    int num = 0;
+
+    printf("\n  ");
+    for (int i = 0; i < this -> SIZE; i++) printf(" %d ", i + 1);
+
+    printf("\n -------------------------\n");
+    while (place){
+        if (num % this -> SIZE == 0) printf("%d", (num >> 3) + 1);
+        if (this -> bw[INT(TURN::black)] & place) printf("|○");
+        else if (this -> bw[INT(TURN::white)] & place) printf("|●");
+        else printf("|  ");
+        if (num % this -> SIZE == 7) printf("|\n -------------------------\n");
+        num++;
+        place = place << 1;
+    }
+
+    return;
+}
+
+osero_genetic::osero_genetic(int player_b, int player_w){
+    this -> bw[INT(TURN::black)] = 0x810000000;
+    this -> bw[INT(TURN::white)] = 0x1008000000;
+    this -> bmethod = player_b;
+    this -> wmethod = player_w;
+    this -> mode = 0;
+
+    return;
+}
+
+osero_genetic::osero_genetic(int player_b, double * eva, int player_w){
+    this -> bw[INT(TURN::black)] = 0x810000000;
+    this -> bw[INT(TURN::white)] = 0x1008000000;
+    this -> bmethod = player_b;
+    this -> wmethod = player_w;
+    for (int i = 0; i < 64; i++)
+        this -> eva[0][i] = eva[i];
+    this -> player = 1;
+    this -> computer = 0;
+    this -> mode = 1;
+
+    return;
+}
+
+osero_genetic::osero_genetic(int player_b, int player_w, double * eva){
+    this -> bw[INT(TURN::black)] = 0x810000000;
+    this -> bw[INT(TURN::white)] = 0x1008000000;
+    this -> bmethod = player_b;
+    this -> wmethod = player_w;
+    for (int i = 0; i < 64; i++)
+        this -> eva[1][i] = eva[i];
+    this -> player = 0;
+    this -> computer = 1;
+    this -> mode = 1;
+
+    return;
+}
+
+osero_genetic::osero_genetic(int player_b, double * eva1, int player_w, double * eva2){
+    this -> bw[INT(TURN::black)] = 0x810000000;
+    this -> bw[INT(TURN::white)] = 0x1008000000;
+    this -> bmethod = player_b;
+    this -> wmethod = player_w;
+    for (int i = 0; i < 64; i++){
+        this -> eva[0][i] = eva1[i];
+        this -> eva[1][i] = eva2[i];
+    }
+    this -> player = 0;
+    this -> computer = 1;
+    this -> mode = 0;
+
+    return;
+}
+
+osero_genetic:: ~osero_genetic(){
+    return;
+}
---
+++ sita/genetic_kaizen/osero_genetic.h
@@ -0,0 +1,71 @@
+#ifndef osero_genetic_h
+#define osero_genetic_h
+
+#include <stdio.h>
+#include <stdlib.h>
+// #include <time.h>
+
+#define INT(num) (static_cast<int>(num))
+#define DOUBLE(num) (static_cast<double>(num))
+
+const bool BLACK = false;
+const bool WHITE = true;
+
+typedef unsigned long BOARD;
+
+enum class TURN{
+    black,
+    white
+};
+
+enum class PLAY_WAY{
+    nhand,
+    random,
+    sentinel
+};
+
+class osero_genetic{
+    private:
+        // other
+        const int SIZE = 8;
+        bool turn = BLACK;
+        int bmethod, wmethod;
+        void (osero_genetic:: * play_method[INT(PLAY_WAY::sentinel)]) (int *, int *) = {
+            &osero_genetic::nhand,
+            &osero_genetic::random
+        };
+
+        // board
+        BOARD bw[2];
+        double eva[2][64];
+
+        // function
+        bool check(BOARD * now, int line, int col);
+        bool check_all(void);
+        int count_last(void);
+        void put(BOARD * now, int line, int col, bool turn);
+        void printb(void);
+        void nhand(int * line, int * col);
+        void random(int * line, int * col);
+        int popcount(BOARD now);
+        double count(BOARD * now);
+        double board_add(BOARD * now, int num, bool turn);
+
+    public:
+        osero_genetic(int player_b, int player_w);
+        osero_genetic(int player_b, double * eva, int player_w);
+        osero_genetic(int player_b, int player_w, double * eva);
+        osero_genetic(int player_b, double * eva1, int player_w, double * eva2);
+        ~osero_genetic();
+
+        static int mode;
+        static int player, computer;
+        static int srand_num;
+        static bool print;
+
+        int read_goal[2] = {1, 1};
+
+        int play(void);
+};
+
+#endif
